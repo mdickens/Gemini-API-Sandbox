2d2460ddebb0db25d20f9b288eb8d7688e9a1f77
Marc <35142431+mdickens@users.noreply.github.com>
2025-06-01 16:45:10 -0500

    promote latest artifacts

--- src/promote.sh
+++ src/promote.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+if [ -z "${GEMINI_API_KEY}" ]; then
+    echo "GEMINI_API_KEY is unset or empty. will not promote until it is set for checks on secret detection"  >> /dev/stderr
+	exit 1;
+fi
+
 if grep $GEMINI_API_KEY * ; then
-	echo "ERROR cannot promot found $GEMINI_API_KEY in file." >> /dev/stderr
+	echo "ERROR cannot promot found GEMINI_API_KEY in file." >> /dev/stderr
 	exit 1;
 else
-	echo "Safe to promot did not find $GEMINI_API_KEY in any file."
+	echo "Safe to promot did not find GEMINI_API_KEY in any file."
 	git add *.py *.sh *.md ;  git commit -m "promote latest artifacts"; git push origin main 
 fi
 

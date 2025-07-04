f9a4cedd4f929dd70434a1284e75e62b2e2365ae
Marc Dickenson <35142431+mdickens@users.noreply.github.com>
2025-07-03 21:23:55 -0500

    Feat: Add unit tests for expanded maze rooms and update test runner

--- src/time-travel-maze/tests/maze.test.js
+++ src/time-travel-maze/tests/maze.test.js
@@ -1,4 +1,4 @@
-const maze = require('../maze.js');
+import maze from '../maze.js';
 
 describe('Maze', () => {
   it('should have a start room', () => {
@@ -17,11 +17,51 @@ describe('Maze', () => {
     expect(maze.historical).toBeDefined();
   });
 
+  it('should have a humorous_encounter room', () => {
+    expect(maze.humorous_encounter).toBeDefined();
+  });
+
+  it('should have a medieval_castle room', () => {
+    expect(maze.medieval_castle).toBeDefined();
+  });
+
+  it('should have a royal_court room', () => {
+    expect(maze.royal_court).toBeDefined();
+  });
+
+  it('should have a royal_dilemma room', () => {
+    expect(maze.royal_dilemma).toBeDefined();
+  });
+
+  it('should have a suspenseful_library room', () => {
+    expect(maze.suspenseful_library).toBeDefined();
+  });
+
   it('should have a futuristic room', () => {
     expect(maze.futuristic).toBeDefined();
   });
 
+  it('should have a futuristic_gadget room', () => {
+    expect(maze.futuristic_gadget).toBeDefined();
+  });
+
+  it('should have a robot_dilemma room', () => {
+    expect(maze.robot_dilemma).toBeDefined();
+  });
+
   it('should have a prehistoric room', () => {
     expect(maze.prehistoric).toBeDefined();
+  });
+
+  it('should have a dinosaur_confrontation room', () => {
+    expect(maze.dinosaur_confrontation).toBeDefined();
+  });
+
+  it('should have a ancient_ruins room', () => {
+    expect(maze.ancient_ruins).toBeDefined();
+  });
+
+  it('should have a nexus_of_time room', () => {
+    expect(maze.nexus_of_time).toBeDefined();
   });
 });

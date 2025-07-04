c0531854dca016c02ca80d13956979ba0abcc484
Marc Dickenson <35142431+mdickens@users.noreply.github.com>
2025-07-04 10:59:30 -0500

    refactor: Refactor maze.test.js to use ES modules and simple assertions

--- src/time-travel-maze/tests/maze.test.js
+++ src/time-travel-maze/tests/maze.test.js
@@ -1,67 +1,29 @@
+import { assert } from './assert.js';
 import maze from '../maze.js';
 
-describe('Maze', () => {
-  it('should have a start room', () => {
-    expect(maze.start).toBeDefined();
-  });
+function testMazeStructure() {
+    assert(maze.start, 'maze.start should be defined');
+    assert(maze.start.description, 'maze.start.description should be defined');
+    assert(maze.start.choices, 'maze.start.choices should be defined');
+    assert(maze.historical, 'maze.historical should be defined');
+    assert(maze.humorous_encounter, 'maze.humorous_encounter should be defined');
+    assert(maze.medieval_castle, 'maze.medieval_castle should be defined');
+    assert(maze.royal_court, 'maze.royal_court should be defined');
+    assert(maze.royal_dilemma, 'maze.royal_dilemma should be defined');
+    assert(maze.suspenseful_library, 'maze.suspenseful_library should be defined');
+    assert(maze.futuristic, 'maze.futuristic should be defined');
+    assert(maze.futuristic_gadget, 'maze.futuristic_gadget should be defined');
+    assert(maze.robot_dilemma, 'maze.robot_dilemma should be defined');
+    assert(maze.prehistoric, 'maze.prehistoric should be defined');
+    assert(maze.dinosaur_confrontation, 'maze.dinosaur_confrontation should be defined');
+    assert(maze.ancient_ruins, 'maze.ancient_ruins should be defined');
+    assert(maze.nexus_of_time, 'maze.nexus_of_time should be defined');
+    console.log('testMazeStructure: PASSED');
+}
 
-  it('should have a start room description', () => {
-    expect(maze.start.description).toBeDefined();
-  });
-
-  it('should have start room choices', () => {
-    expect(maze.start.choices).toBeDefined();
-  });
-
-  it('should have a historical room', () => {
-    expect(maze.historical).toBeDefined();
-  });
-
-  it('should have a humorous_encounter room', () => {
-    expect(maze.humorous_encounter).toBeDefined();
-  });
-
-  it('should have a medieval_castle room', () => {
-    expect(maze.medieval_castle).toBeDefined();
-  });
-
-  it('should have a royal_court room', () => {
-    expect(maze.royal_court).toBeDefined();
-  });
-
-  it('should have a royal_dilemma room', () => {
-    expect(maze.royal_dilemma).toBeDefined();
-  });
-
-  it('should have a suspenseful_library room', () => {
-    expect(maze.suspenseful_library).toBeDefined();
-  });
-
-  it('should have a futuristic room', () => {
-    expect(maze.futuristic).toBeDefined();
-  });
-
-  it('should have a futuristic_gadget room', () => {
-    expect(maze.futuristic_gadget).toBeDefined();
-  });
-
-  it('should have a robot_dilemma room', () => {
-    expect(maze.robot_dilemma).toBeDefined();
-  });
-
-  it('should have a prehistoric room', () => {
-    expect(maze.prehistoric).toBeDefined();
-  });
-
-  it('should have a dinosaur_confrontation room', () => {
-    expect(maze.dinosaur_confrontation).toBeDefined();
-  });
-
-  it('should have a ancient_ruins room', () => {
-    expect(maze.ancient_ruins).toBeDefined();
-  });
-
-  it('should have a nexus_of_time room', () => {
-    expect(maze.nexus_of_time).toBeDefined();
-  });
-});
+try {
+    testMazeStructure();
+    console.log('All maze tests passed!');
+} catch (error) {
+    console.error('Maze test failed:', error.message);
+}

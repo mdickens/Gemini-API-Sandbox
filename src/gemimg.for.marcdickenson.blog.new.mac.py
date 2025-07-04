9ab67612251af2a7e7b5a02c777ff47819910b85
Marc Dickenson <35142431+mdickens@users.noreply.github.com>
2025-07-03 20:17:41 -0500

    promote latest artifacts

---
+++ src/gemimg.for.marcdickenson.blog.new.mac.py
@@ -0,0 +1,15 @@
+from google import genai
+from google.genai import types
+import sys
+import my_gemini
+from PIL import Image
+
+
+client = my_gemini.getClient()
+
+image = Image.open(sys.argv[1])
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=[image, f"Tell me about this image in scientific detail, in 200 words or less and output response in html format ready to post in my blog. Do not prefix the html output wiwith any other headers or captions that are not html. Only html output is allowed."]
+)
+print(response.text)

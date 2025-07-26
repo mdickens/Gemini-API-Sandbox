3b0703ed3b6ff04210f776b0c4f69b7d66bf4121
Marc Dickenson <35142431+mdickens@users.noreply.github.com>
2025-07-26 13:01:01 -0500

    add copy script

--- src/chess/copy.to.dl.sh
+++ src/chess/copy.to.dl.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -x 
-rm -rf ~/Downloads/*.mp3 ~/Downloads/index.html  ~/Downloads/tests ~/Downloads/script.js ~/Downloads/style.css ~/Downloads/script.js; cp -p -f -v -r *.html *.js *.mp3 *.json *.css tests ~/Downloads  
+#rm -rf ~/Downloads/modules ~/Downloads/*.mp3 ~/Downloads/index.html  ~/Downloads/tests ~/Downloads/script.js ~/Downloads/style.css ~/Downloads/script.js; cp -p -f -v -r *.html *.js *.mp3 *.json *.css tests modules ~/Downloads  
+rm -rf ~/Downloads/chess
+mkdir ~/Downloads/chess
+cp -p -f -v -r * ~/Downloads/chess
+
